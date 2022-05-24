83e79d035feb126f586859b2e63171d171b27c61
rodolfo-kunzel <rodi.bkunzel@gmail.com>
2022-05-24 16:15:46 -0300

    register-controller
    Register controller done

--- lib/my_app_widget.dart
+++ lib/my_app_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vr_project/features/authentication/register/view/register_page.dart';
 import 'package:vr_project/table_calendar_examples/start_page.dart';
-import 'home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,21 +8,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const RegiterPage(),
+      home: RegiterPage(),
     );
   }
 }

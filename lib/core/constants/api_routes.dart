84e7c25ee8ea68d843fd8784608861545b2748bb
rodolfo-kunzel <rodi.bkunzel@gmail.com>
2022-05-24 11:32:30 -0300

    constants-models-recouce
    Constants, models and resorces updated

---
+++ lib/core/constants/api_routes.dart
@@ -0,0 +1,7 @@
+class ApiRoutes {
+  static const baseUrl = "https://academy-auth.herokuapp.com";
+
+  static const loginRoute = "$baseUrl/login";
+
+  static const registerRoute = "$baseUrl/register";
+}

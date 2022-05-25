6e24e1aeef6ed0087639f742191964db9e51725c
Victoriurki <101892924+Victoriurki@users.noreply.github.com>
2022-05-25 14:26:24 -0300

    Breaking my head on my home page

--- lib/my_app_widget.dart
+++ lib/my_app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vr_project/features/authentication/register/view/register_page.dart';
 import 'package:vr_project/table_calendar_examples/start_page.dart';
-import 'home_page.dart';
+import 'my_home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
